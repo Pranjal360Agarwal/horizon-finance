bc5ae7ebb96a19b0ef7b2f6f33e970a112194bd5
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-09-17 18:11:59 +0530

    Fix the bugs

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,10 @@
 
 import { getLoggedInUser } from "@/lib/actions/user.actions";
-import Image from "next/image";
+
 import { redirect } from "next/navigation";
 
 export default async function RootLayout({
-  children,
+
 }: Readonly<{
   children: React.ReactNode;
 }>) {
@@ -12,19 +12,5 @@ export default async function RootLayout({
 
   if(!loggedIn) redirect('/sign-in')
 
-  return (
-    <main className="flex h-screen w-full font-inter">
-      <Sidebar user={loggedIn} />
-
-      <div className="flex size-full flex-col">
-        <div className="root-layout">
-          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
-          <div>
-            <MobileNav user={loggedIn} />
-          </div>
-        </div>
-        {children}
-      </div>
-    </main>
-  );
+ 
 }
