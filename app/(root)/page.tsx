6a222618e82d5d4cca29e3329091ccc2e86a4d19
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-09-18 16:44:22 +0530

    Update the Home page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,37 @@ const Home = () => {
  
 
   return (
-   <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
+        </header>
+
+        <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
+    </section>
   )
 }
 
