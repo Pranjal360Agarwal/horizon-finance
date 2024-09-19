49d28f2e272338ce2617014df322bc356e37414e
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2024-09-19 10:30:46 +0530

    Add the layout for the sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,11 @@
-
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
-
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function RootLayout({
-
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
@@ -12,5 +13,19 @@ export default async function RootLayout({
 
   if(!loggedIn) redirect('/sign-in')
 
- 
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
+  );
 }
